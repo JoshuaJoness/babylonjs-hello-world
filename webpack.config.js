aefd851960265172a44b91d708e40d49a0f2b67b
Joshua Jones <joshuajonesdev@gmail.com>
2021-02-20 19:53:47 -0500

    configured webpack, Babylon app now running locally

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,19 @@ const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
-
 module.exports = {
-    entry: path.resolve(appDirectory, "src/app.ts"),
+    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
     output: {
-        filename: "js/bundleName.js"
+        filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
     },
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
     },
     devServer: {
         host: "0.0.0.0",
-        port: 8080,
-        disabledHostCheck: true,
-        contentBase: path.resolver(appDirectory, "public"),
+        port: 8080, //port that we're using for local host (localhost:8080)
+        disableHostCheck: true,
+        contentBase: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
         publicPath: "/",
         hot: true,
     },
@@ -25,9 +24,9 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 use: "ts-loader",
-                excludes: /node_modules/,
-            }
-        ]
+                exclude: /node_modules/,
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -37,4 +36,4 @@ module.exports = {
         new CleanWebpackPlugin(),
     ],
     mode: "development",
-}
+};
